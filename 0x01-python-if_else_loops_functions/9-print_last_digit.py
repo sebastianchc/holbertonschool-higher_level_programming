e9618a1081ed66da864f8e6219783a81ff0d8f2e
sebastianchc <1200@holbertonschool.com>
2019-12-11 02:55:34 +0000

    Print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last = number % 10
+    else:
+        last = number % -10
+        last *= -1
+    print("{}" .format(last), end='')
+    return last
