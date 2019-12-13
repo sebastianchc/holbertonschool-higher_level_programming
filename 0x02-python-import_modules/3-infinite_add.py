b7e02a7ad15328a52a727fdaae3658ca93d95858
sebastianchc <1200@holbertonschool.com>
2019-12-13 00:41:07 +0000

    Infinity add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    n = 1
+    argc = len(argv) - 1
+    if len(argv) == 1:
+        print("{} arguments." .format(argc))
+    elif len(argv) == 2:
+        print("{} argument:" .format(argc))
+        print("{}: {}" .format(argc, argv[1]))
+    else:
+        print("{} arguments:" .format(argc))
+        for arguments in range(1, len(argv)):
+            print("{}: {}" .format(n, argv[n]))
+            n += 1
