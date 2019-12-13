e50105c3366479acc255431bcd55172a7c274b9a
sebastianchc <1200@holbertonschool.com>
2019-12-13 03:25:24 +0000

    [Update]Testing operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+from sys import argv
 if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{} + {} = {}" .format(a, b, add(a, b)))
-    print("{} - {} = {}" .format(a, b, sub(a, b)))
-    print("{} * {} = {}" .format(a, b, mul(a, b)))
-    print("{} / {} = {}" .format(a, b, div(a, b)))
+    if len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+        if operator == '+':
+            print("{} + {} = {}" .format(a, b, add(a, b)))
+        elif operator == '-':
+            print("{} - {} = {}" .format(a, b, sub(a, b)))
+        elif operator == '*':
+            print("{} * {} = {}" .format(a, b, mul(a, b)))
+        elif operator == '/':
+            print("{} / {} = {}" .format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
