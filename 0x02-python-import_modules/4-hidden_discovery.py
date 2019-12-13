48d37b8912997e83496bdaf1a2fe9c4dc5b7184c
sebastianchc <1200@holbertonschool.com>
2019-12-13 01:16:49 +0000

    [Update]Using dir()

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+#!/usr/bin/python3.4
+import hidden_4
 if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{} + {} = {}" .format(a, b, add(a, b)))
-    print("{} - {} = {}" .format(a, b, sub(a, b)))
-    print("{} * {} = {}" .format(a, b, mul(a, b)))
-    print("{} / {} = {}" .format(a, b, div(a, b)))
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
