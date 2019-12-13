c48b475d530578482d01974800b7aac1b56788b9
sebastianchc <1200@holbertonschool.com>
2019-12-13 00:49:51 +0000

    [Update]Infinity add(python is amazing)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    n = 1
-    argc = len(argv) - 1
-    if len(argv) == 1:
-        print("{} arguments." .format(argc))
-    elif len(argv) == 2:
-        print("{} argument:" .format(argc))
-        print("{}: {}" .format(argc, argv[1]))
+    if len(argv) < 2:
+        print("{}" .format(len(argv) - 1))
     else:
-        print("{} arguments:" .format(argc))
-        for arguments in range(1, len(argv)):
-            print("{}: {}" .format(n, argv[n]))
-            n += 1
+        n = int(argv[1])
+        for numbers in range(2, len(argv)):
+            n += int(argv[numbers])
+        print("{}" .format(n))
