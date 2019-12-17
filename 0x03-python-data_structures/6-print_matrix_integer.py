3e3d7021bbc6b8000620a457da6d15e273b2b580
sebastianchc <1200@holbertonschool.com>
2019-12-17 23:07:31 +0000

    [Update] Printing a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for integer in row:
+            if integer != row[-1]:
+                print("{:d} " .format(integer), end='')
+            else:
+                print("{:d}" .format(integer))
