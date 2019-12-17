061876eaa0f714a8b2685e0ae212ba25d8aab6e5
sebastianchc <1200@holbertonschool.com>
2019-12-17 22:15:03 +0000

    [Update] Using list.reverse() and considering an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    integer = -1
-    for rev in range(0, len(my_list)):
-        print("{:d}" .format(my_list[integer]))
-        integer -= 1
+    if my_list:
+        list.reverse(my_list)
+        for integer in my_list:
+            print("{:d}" .format(integer))
