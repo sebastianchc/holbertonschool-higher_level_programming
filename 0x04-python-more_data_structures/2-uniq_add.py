670c8b7a45dc053f9168e14329919f1646085677
sebastianchc <1200@holbertonschool.com>
2019-12-20 00:16:45 +0000

    [Fix] Better set()

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = dict.fromkeys(my_list)
+    new_list = set(my_list)
     result = 0
     for integer in new_list:
         result += integer
