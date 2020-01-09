d362048188d6fcca389cb7c8d0b7bce1f1ba6dba
sebastianchc <1200@holbertonschool.com>
2020-01-09 01:45:04 +0000

    [Fix] Please PEP8!

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    cont = 0
     for i in range(x):
         try:
             print("{:d}" .format(my_list[i]), end="")
-            count += 1
+            cont += 1
         except (ValueError, TypeError):
             pass
     print("")
-    return count
+    return cont
