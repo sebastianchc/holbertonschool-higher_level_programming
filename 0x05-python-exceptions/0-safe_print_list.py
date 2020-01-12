72412efc243ba800963fa464acbd389e5c6cbfbe
sebastianchc <1200@holbertonschool.com>
2020-01-12 22:01:57 +0000

    [Fix] Iterating

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        count = 0
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-    except:
-        i -= 1
+            count += 1
+    except IndexError:
         pass
-    print()
-    return i + 1
+    print("")
+    return count
