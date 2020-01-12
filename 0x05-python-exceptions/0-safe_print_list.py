954c22fbdb0bc8b779fb19a6b2a4c8036c8866f9
sebastianchc <1200@holbertonschool.com>
2020-01-12 22:08:27 +0000

    [Fix] Empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
+            if count >= x:
+                break
     except IndexError:
         pass
     print("")
