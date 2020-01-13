7cbecd3c6f0a2ced5e68f96a581b3a8fa041f132
sebastianchc <1200@holbertonschool.com>
2020-01-13 00:43:05 +0000

    [Update] Using exception as exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import sys
-
-
 def safe_print_integer_err(value):
     try:
-        print("{:d}" .format(value))
+        print("{:d}".format(value))
         return True
-    except Exception as Error:
-        print("Exception: {:d}".format(Error), file=sys.stderr)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
