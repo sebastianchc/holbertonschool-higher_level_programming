cf370dde20aa1d67764cc4b4ba13b2396f4d9340
sebastianchc <1200@holbertonschool.com>
2020-01-14 22:10:55 +0000

    [Update] More methods!

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if not self.__size:
+            print("")
+        else:
+            for row in range(self.__size):
+                print("#" * self.__size)
