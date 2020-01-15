41be1a64379727b00071af67c557f7d3b283b463
sebastianchc <1200@holbertonschool.com>
2020-01-15 13:49:55 +0000

    [Fix] Shit language

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 class Rectangle:
 
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
     @property
     def width(self):
         return self.__width
@@ -9,22 +13,20 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        else:
+            self.__height = value
