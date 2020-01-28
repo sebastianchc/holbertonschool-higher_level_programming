217c719ec01cf4f85f6e3c9f48e605f0977da091
sebastianchc <1200@holbertonschool.com>
2020-01-28 02:45:32 +0000

    [Update] Adding dictionary public method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = value
@@ -30,7 +30,7 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = value
@@ -95,6 +95,11 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        dictionary = {"x": self.x, "y": self.y, "id": self.id,
+                      "height": self.height, "width": self.width}
+        return dictionary
 
     def __str__(self):
         string = "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"
