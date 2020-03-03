3e4762d9eeab94637d9559d824ab6ecada594077
sebastianchc <1200@holbertonschool.com>
2020-03-03 17:38:15 -0500

    Fixing task 1 try 3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create a new user
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
-      IDENTIFIED by user_0d_1_pwd;
+-- And give all the permissions
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
