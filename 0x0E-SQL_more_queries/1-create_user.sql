c750d0b51a58f6c6c168789cfc3b75f1c44c01ba
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-04 14:55:55 +0000

    [Update] Create user with all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,2 @@
--- Create a new user
--- And give all the permissions
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+-- Create a user with all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
