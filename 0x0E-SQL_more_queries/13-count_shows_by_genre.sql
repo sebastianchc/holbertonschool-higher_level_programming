ec44184c72d76f2a8af8bc953918f05ff5a41750
sebastianchc <1200@holbertonschool.com>
2020-03-28 09:51:13 -0500

    [Fix] Changing GROUP BY value

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- List all shows contained in hbtn_0d_tvshows
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
