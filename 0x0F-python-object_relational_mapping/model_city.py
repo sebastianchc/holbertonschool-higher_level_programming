e283fac1c40d6b4cbac1ac5552eed41940e77000
sebastianchc <1200@holbertonschool.com>
2020-03-29 10:16:00 -0500

    [Update] Adding task 14 Adding model_city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" List all states """
+
+from sqlalchemy import ForeignKey, Column, Integer, String
+from model_state import Base
+
+
+class City(Base):
+    """ Class State """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
