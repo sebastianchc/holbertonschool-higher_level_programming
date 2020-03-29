fc3deccd7842fa4576367ec2bb5cd154b0968b27
sebastianchc <1200@holbertonschool.com>
2020-03-28 20:10:22 -0500

    [Update] Adding task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" List all states """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=argv[1],
+                               passwd=argv[2],
+                               db=argv[3])
+    cursor = database.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities "
+    query = query + "JOIN states ON cities.state_id=states.id "
+    query = query + "ORDER BY cities.id ASC;"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
