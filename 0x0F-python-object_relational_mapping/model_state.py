7ebee9e6a2d8e941abe063d205ba89a9cdfdba3e
sebastianchc <1200@holbertonschool.com>
2020-03-28 22:17:36 -0500

    [Update] Adding model_state task

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" List all states """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ Class State """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
