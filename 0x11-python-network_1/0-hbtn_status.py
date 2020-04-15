f04f1ead9abb0d509a3d66a3d39d7962a52a5be9
sebastianchc <1200@holbertonschool.com>
2020-04-15 15:56:26 -0500

    [Fix] PEP8 style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@ from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-   with urlopen("https://intranet.hbtn.io/status") as response:
-      html = response.read()
-      print("Body response:")
-      print("\t- type: {}".format(type(html)))
-      print("\t- content: {}".format(html))
-      print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    with urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
