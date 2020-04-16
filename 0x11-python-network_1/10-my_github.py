b7a7acea976fca100d18802258c8fa725c962b55
sebastianchc <1200@holbertonschool.com>
2020-04-16 10:15:07 -0500

    [Update] GitHub api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from requests import post
+from requests import get
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
 if __name__ == "__main__":
-   
+    url = "https://api.github.com/user"
+    request = get(url, auth=(argv[1], argv[2]))
+    js = request.json()
+    print(js.get("id"))
