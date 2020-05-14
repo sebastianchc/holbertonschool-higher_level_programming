62d39843bcc10526c5dd05bee0e35b819c8af124
sebastianchc <1200@holbertonschool.com>
2020-05-14 14:09:20 -0500

    Without with

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-with(myObject) {
-  myObject.incr = function () {
-    myObject.value = value + 1
-  }
-}
+myObject.incr = function () {
+  myObject.value = myObject.value + 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
