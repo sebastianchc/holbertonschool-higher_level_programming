e36c4cb19722d0d8bbf93e34fe072b37ddd1a15a
sebastianchc <1200@holbertonschool.com>
2020-05-14 17:51:24 -0500

    Semistandard style

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const new_list = list.map(function (num, index) {
+const newList = list.map(function (num, index) {
   return index * num;
 });
 console.log(list);
-console.log(new_list);
+console.log(newList);
