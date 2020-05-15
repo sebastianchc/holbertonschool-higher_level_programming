a54b79c1f6bb1449c0cab35f51b801879d45fedc
sebastianchc <1200@holbertonschool.com>
2020-05-14 22:42:04 -0500

    Iterate API

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (err, res, cont) {
+  if (!err) {
+    let count = 0;
+    const json = JSON.parse(cont);
+    const list = json.results;
+    for (const movie of list) {
+      const characters = movie.characters;
+      for (const character of characters) {
+	if (character.search('18') !== -1) {
+	  count++;
+	}
+      }
+    }
+    console.log(count);
+  }
+});
