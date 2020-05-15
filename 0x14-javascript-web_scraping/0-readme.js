bc6e873936278d2e5f8344ad447f081bbd8f797b
sebastianchc <1200@holbertonschool.com>
2020-05-14 21:36:15 -0500

    Semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = fs.readFile(process.argv[2], 'utf8', function(err, file) {
+fs.readFile(process.argv[2], 'utf8', function (err, file) {
   if (err) {
     console.log(err);
   } else {
