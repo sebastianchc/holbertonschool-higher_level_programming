202ee749f1af37da751cdd2922b2765911a30676
sebastianchc <1200@holbertonschool.com>
2020-05-14 22:13:19 -0500

    Make request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function(err, res, cont) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
+});
