68e72fcbd47906fda13b4556b905464fdf309492
sebastianchc <1200@holbertonschool.com>
2020-05-14 22:23:51 -0500

    Request API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, cont) {
+  if (!err) {
+    const json = JSON.parse(cont);
+    console.log(json.title);
+  }
+});
