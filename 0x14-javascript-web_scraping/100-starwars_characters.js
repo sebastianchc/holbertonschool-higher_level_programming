109f087c3511d64e3239862e761716df3b64f7e6
sebastianchc <1200@holbertonschool.com>
2020-05-17 10:44:25 -0500

    Film characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, cont) {
+  if (!err) {
+    const json = JSON.parse(cont);
+    const characters = json.characters;
+    for (const character of characters) {
+      request(character, function (err, res, cont) {
+	if (!err) {
+	  const json = JSON.parse(cont);
+	  console.log(json.name);
+	}
+      });
+    }
+  }
+});
