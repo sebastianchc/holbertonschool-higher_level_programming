5fcbb9aecc35e579da925a4ac0574bfb9f5c93e3
sebastianchc <1200@holbertonschool.com>
2020-05-17 10:46:53 -0500

    Semistandard style

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,10 +6,10 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
     const characters = json.characters;
     for (const character of characters) {
       request(character, function (err, res, cont) {
-	if (!err) {
-	  const json = JSON.parse(cont);
-	  console.log(json.name);
-	}
+        if (!err) {
+          const json = JSON.parse(cont);
+          console.log(json.name);
+        }
       });
     }
   }
